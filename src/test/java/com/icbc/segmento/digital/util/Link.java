b32057eb6dcb047f1c1bd49996446f5214dbd449
Javier Ramonda <javier.ramonda@icbc.com.ar>
2021-05-13 12:46:00 -0300

    Fix url in Links file

--- src/test/java/com/icbc/segmento/digital/util/Link.java
+++ src/test/java/com/icbc/segmento/digital/util/Link.java
@@ -1,7 +1,10 @@
 package com.icbc.segmento.digital.util;
 
+import com.icbc.segmento.digital.util.url.Environment;
+import com.icbc.segmento.digital.util.url.Url;
+
 public class Link {
 	
-    public static final String TRANSFERENCE = "https://mds-channel-transfer-account-1-0-fbd.apps.ocp4-tst.intranet.local/channel-transfer-account/1.0/get-destination-info";
+    public static final String TRANSFERENCE = Url.UrlFactory.load("channel-transfer-account", "get-destination-info", "1.0", Environment.FBD);
 
 }
